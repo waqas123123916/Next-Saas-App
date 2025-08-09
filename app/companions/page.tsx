cddd16c447bb2276f839a87bb55021a06ed9f5f7
waqas123123916 <waqas@cosmicsync.co>
2025-08-09 13:19:31 +0500

    feat: Add CompanionCard, SearchInput, and SubjectFilter components with filtering functionality for companions

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,8 +1,35 @@
+import CompanionCard from '@/components/CompanionCard';
+import SearchInput from '@/components/SearchInput';
+import SubjectFilter from '@/components/SubjectFilter';
+import { getAllCompanions } from '@/lib/actions/companions.action';
+import { getSubjectColor } from '@/lib/utils';
 import React from 'react'
 
-const page = () => {
+const page = async({searchParams}:SearchParams) => {
+ const filter = await searchParams;
+  const subject =  filter.subject ? filter.subject : '';
+  const topic = filter.topic ? filter.topic : '';
+
+  const companions = await getAllCompanions({ subject, topic });
+  console.log("Companions:", companions);
   return (
-    <div>Companions Library</div>
+   <main>
+    <section className="flex justify-between gap-4 max-sm:flex-col">
+      <h1>Companion Library</h1>
+      <div className="flex gap-4">
+        <SearchInput/>
+        <SubjectFilter/>
+      </div>
+    </section>
+    <div className="companions-grid">
+      {
+        companions.map((companion) => (
+          <CompanionCard key={companion.id} {...companion} color={getSubjectColor(companion.subject)} />
+        ))
+      }
+    </div>
+
+   </main>
   )
 }
 
