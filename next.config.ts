b4fbe77369bcb715e79c4cbb3ebb9aa29dd38523
waqas123123916 <waqas@cosmicsync.co>
2025-08-12 13:08:39 +0500

    feat: Implement CompanionSession and CompanionComponent with voice interaction capabilities

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images:{
+    remotePatterns:[
+      { hostname: 'img.clerk.com'}
+    ]
+  }
 };
 
 export default nextConfig;
