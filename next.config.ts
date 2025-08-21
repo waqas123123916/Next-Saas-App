018ab7f223743136fdfc49eb76124e0c4750e432
waqas123123916 <waqas@cosmicsync.co>
2025-08-21 14:33:12 +0500

    feat: add ESLint configuration to ignore errors during builds

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   typescript:{
     ignoreBuildErrors:true
   },
+  eslint:{
+    ignoreDuringBuilds:true
+  },
   /* config options here */
   images:{
     remotePatterns:[
