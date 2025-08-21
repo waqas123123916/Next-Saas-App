d6aabe8b649dd0ee6946ca75a57559a1aa363796
waqas123123916 <waqas@cosmicsync.co>
2025-08-21 14:32:02 +0500

    feat: enable TypeScript build error ignoring in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import {withSentryConfig} from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  typescript:{
+    ignoreBuildErrors:true
+  },
   /* config options here */
   images:{
     remotePatterns:[
